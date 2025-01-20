ce2a888154195300941190c2f95a37ca9a947540
Rodion <new.rodik@gmail.com>
2025-01-20 22:35:23 +0300

    Editing a project

--- src/main.py
+++ src/main.py
@@ -1,1 +1,6 @@
-print("Hello world");
+from utils import *
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+result = add_numbers(num1, num2)
+
+print("Результат сложения:", result)
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,4 @@
-import zipfile
-# Сжатие и распаковка файлов
-def zip_files(file_names, zip_name):
-    with zipfile.ZipFile(zip_name, 'w') as zipf:
-        for file in file_names:
-            zipf.write(file)
-
-def unzip_file(zip_name, extract_to):
-    with zipfile.ZipFile(zip_name, 'r') as zipf:
-        zipf.extractall(extract_to)
+def add_numbers(n1, n2):
+    return n1 + n2
+def multiply_numbers (n1, n2):
+    return n1 + n2
