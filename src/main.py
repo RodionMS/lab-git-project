73da64d76efdcc38fd6fa1421592248a4163123c
Rodion <new.rodik@gmail.com>
2025-01-20 22:57:58 +0300

    Editing multiply 1

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from utils import *
 num1 = float(input("Введите первое число: "))
 num2 = float(input("Введите второе число: "))
-result = add_numbers(num1, num2)
-
-print("Результат сложения:", result)
+result = multiply_numbers(num1, num2)
+print("Результат умножения:", result)
