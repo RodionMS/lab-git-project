b7672ec9222d345dbf6e4d3630561e3519e146c7
Rodion <new.rodik@gmail.com>
2025-01-21 00:11:23 +0300

    Editing add-numbers 2

--- src/main.py
+++ src/main.py
@@ -2,5 +2,4 @@ from utils import *
 num1 = float(input("Введите первое число: "))
 num2 = float(input("Введите второе число: "))
 result = add_numbers(num1, num2)
-
 print("Результат сложения:", result)
